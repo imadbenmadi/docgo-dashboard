326868a121ed9ffa46ef0394de517bb549e60353
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2025-07-08 20:27:06 +0100

    let's start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex text-amber-300 items-center justify-center h-screen bg-gray-100 font-sans">
+      hello
+    </div>
+  );
+}
+
+export default App;
